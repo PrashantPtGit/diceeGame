bb6920cf7e52eb2b4cb514b13c48468a1c46e769
PrashantPtGit <prashantpatitripathi@gmail.com>
2022-09-28 09:19:25 +0530

    fouth commit

--- script.js
+++ script.js
@@ -30,10 +30,10 @@ document.getElementById("image2").src = randomImage2;
 // who wins 
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 wins";
+  document.querySelector("h1").innerHTML = userName1 + " wins";
 
 } else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 wins";
+  document.querySelector("h1").innerHTML = userName2 + " wins";
 
 } else {
   document.querySelector("h1").innerHTML = "Draw";
